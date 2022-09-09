e083913d3f875380b6c95063bf9e797abff8030b
corno <corno.astn.a@schraverus.com>
2022-09-09 21:59:51 +0200

    .

--- pub/src/imp/public/f_createSortedForEach.ts
+++ pub/src/imp/public/f_createSortedForEach.ts
@@ -1,10 +1,43 @@
+import * as pt from "pareto-core-types"
+import * as pi from "pareto-core-internals"
+
 import * as api from "api-pareto-collation"
 
-export const f_createSortedForEach: api.XCreateSortedForEach = ($d) => {
-    return ($, $i) => {
-        $.forEach(
-            (a, b) => $d.isYinBeforeYang({ yin: b, yang: a }),
-            $i
-        )
+
+function imp<T>(
+    $d: {
+        isYinBeforeYang: api.FIsYinBeforeYang
     }
+): api.PSortedForEach {
+
+    return <T>(
+        $: pt.Dictionary<T>,
+        $i: ($: {
+            readonly "entry": T;
+            readonly "key": string;
+        }) => void
+    ) => {
+        const temp: { key: string, value: T }[] = []
+
+        $.map(($, key) => {
+            temp.push({
+                key: key,
+                value: $,
+            })
+        })
+        temp.sort((a, b) => $d.isYinBeforeYang({ yin: b.key, yang: a.key }) ? 1 : -1).forEach(($) => {
+            $i({
+                key: $.key,
+                entry: $.value,
+            })
+        })
+
+        //     (a, b) => $d.isYinBeforeYang({ yin: b, yang: a }),
+        //     $i
+        // )
+    }
+}
+
+export const f_createSortedForEach: api.FCreateSortedForEach = ($d) => {
+    return imp($d)
 }
--- test/src/bin/test.ts
+++ test/src/bin/test.ts
@@ -7,25 +7,25 @@ import * as test from "lib-pareto-test"
 import * as diff from "res-pareto-diff"
 import * as fs from "res-pareto-filesystem"
 
-import { createGetTestset } from "../imp"
+import { f_createGetTestset } from "../imp"
 import { dependencies } from "../dependencies/dependencies"
 
 
-pe.runProgram(
-    test.createTester(
-        {
-            getTestSet: createGetTestset(
-                dependencies
-            ),
-            diff: {
-                diffData: diff.diffData,
-                stringsAreEqual: diff.stringsAreEqual,
-            },
-            fs: {
-                readFile: fs.readFile,
-                writeFile: fs.writeFile,
-                unlink: fs.unlink,
-            },
-        },
-    )
-)
+// pe.runProgram(
+//     test.createTester(
+//         {
+//             getTestSet: createGetTestset(
+//                 dependencies
+//             ),
+//             diff: {
+//                 diffData: diff.diffData,
+//                 stringsAreEqual: diff.stringsAreEqual,
+//             },
+//             fs: {
+//                 readFile: fs.readFile,
+//                 writeFile: fs.writeFile,
+//                 unlink: fs.unlink,
+//             },
+//         },
+//     )
+// )
