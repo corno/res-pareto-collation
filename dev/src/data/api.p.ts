6e7d2f848b9f4b321cb5426908e5632620bfbf23
corno <corno.astn.a@schraverus.com>
2023-01-16 21:19:51 +0100

    .

---
+++ dev/src/data/api.p.ts
@@ -0,0 +1,57 @@
+import * as pr from "pareto-core-raw"
+import {
+    externalReference as er,
+    string as str,
+    nullType,
+    type,
+    reference as ref,
+    boolean as bln,
+    array, dictionary, group, member, taggedUnion, types, _function
+} from "lib-pareto-typescript-project/dist/modules/glossary/api/shorthands.p"
+
+
+import { string, reference, externalReference, number, boolean } from "lib-pareto-typescript-project/dist/modules/moduleDefinition/api/shorthands.p"
+import * as mproject from "lib-pareto-typescript-project/dist/modules/project"
+import * as mmoduleDefinition from "lib-pareto-typescript-project/dist/modules/moduleDefinition"
+const d = pr.wrapRawDictionary
+
+export const $: mmoduleDefinition.TModuleDefinition = {
+    'glossary': {
+        'imports': d({
+            // "project": "../../project",
+            // "common": "glo-pareto-common",
+            // "main": "glo-pareto-main",
+        }),
+        'namespace': {
+            'types': types({
+                "StringPair": group({
+                    "a": member(str()),
+                    "b": member(str()),
+                })
+            }),
+            'interfaces': d({}),
+
+        },
+        'functions': d({
+            "IsABeforeB": {
+                'data': reference("StringPair"),
+                'return value': boolean()
+            }
+        }),
+        'callbacks': d({}),
+        'pipes': d({}),
+    },
+    'api': {
+        'imports': d({
+            // "main": "glo-pareto-main",
+        }),
+        'algorithms': d({
+            "localeIsABeforeB": {
+                'definition': ['function', {
+                    'function': "IsABeforeB"
+                }],
+                'type': ['reference', null],
+            },
+        })
+    },
+}
--- dev/src/data/project.p.ts
+++ dev/src/data/project.p.ts
@@ -1,57 +1,16 @@
 import * as pr from "pareto-core-raw"
-import {
-    externalReference as er,
-    string as str,
-    reference as ref,
-    boolean as bln,
-} from "lib-pareto-typescript-project/dist/modules/glossary/api/shorthands.p"
-import { dictionary, group, member, taggedUnion, types, _function } from "lib-pareto-typescript-project/dist/modules/glossary/api/shorthands.p"
 
+import * as mproject from "lib-pareto-typescript-project/dist/modules/project"
 
-import { string, reference, externalReference, number, boolean } from "lib-pareto-typescript-project/dist/modules/api/api/shorthands.p"
-import * as NProject from "lib-pareto-typescript-project/dist/modules/project"
-const wd = pr.wrapRawDictionary
-export const project: NProject.TProject = {
+import { $ as api } from "./api.p"
+
+const d = pr.wrapRawDictionary
+
+export const $: mproject.TProject = {
     'type': ['resource', null],
-    'modules': wd({
+    'modules': d({
         "main": {
-            'definition': {
-                'glossary': {
-                    'imports': wd({
-                        // "project": "../../project",
-                        // "common": "glo-pareto-common",
-                        // "main": "glo-pareto-main",
-                    }),
-                    'types': types({
-                        "StringPair": group({
-                            "a": member(str()),
-                            "b": member(str()),
-                        })
-                    }),
-                    'functions': wd({
-                        "IsABeforeB": {
-                            'data': reference("StringPair"),
-                            'return value': boolean()
-                        }
-                    }),
-                    'interfaces': wd({}),
-                    'callbacks': wd({}),
-                    'pipes': wd({}),
-                },
-                'api': {
-                    'imports': wd({
-                        // "main": "glo-pareto-main",
-                    }),
-                    'algorithms': wd({
-                        "localeIsABeforeB": {
-                            'definition': ['function', {
-                                'function': "IsABeforeB"
-                            }],
-                            'type': ['reference', null],
-                        },
-                    })
-                },
-            },
+            'definition': api,
             'implementation': {}
 
         },
