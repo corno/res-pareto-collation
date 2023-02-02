19a5cd2ea1a2fcde9e73f6933f64a473b70e4d23
corno <corno.astn.a@schraverus.com>
2023-02-02 10:50:00 +0100

    .

--- pareto/src/data/project.p.ts
+++ pareto/src/data/project.p.ts
@@ -7,8 +7,6 @@ import { $ as api } from "./api.p"
 const d = pr.wrapRawDictionary
 
 export const $: mproject.TProject = {
-    'name': "res-pareto-collation",
-
     'author': "Corno",
     'description': "a pareto wrapper around collation functions as Pareto lacks them. collation functions are used for ordering strings",
     'license': "ISC",
