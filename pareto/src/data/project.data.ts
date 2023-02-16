fdf0b19d2c6300de68af0f426a546fa16eedcea9
corno <corno.astn.a@schraverus.com>
2023-02-16 09:45:17 +0100

    .

--- pareto/src/data/project.data.ts
+++ pareto/src/data/project.data.ts
@@ -11,17 +11,25 @@ export const $: mproject.T.Project = {
     'description': "a pareto wrapper around collation functions as Pareto lacks them. collation functions are used for ordering strings",
     'license': "ISC",
 
-    'pubdependencies': d({
+    'dependencies': d({
         "glo-pareto-common": {},
     }),
     'type': ['resource', {
         'definition': api,
+        'nativeDependencies': d({}),
         'devDependencies': d({
             "@types/node": {},
         }),
         'test': {
             'dependencies': d({
             }),
+            'glossary': {
+                'functions': d({}),
+                'imports': d({}),
+                'parameters': d({}),
+                'types': d({}),
+                'interfaces': d({}),
+            },
         }
     }],
 }
