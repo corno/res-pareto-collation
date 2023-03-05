fb2aeada1ca6a4afe39393d8ade7fac42dfeee12
corno <corno.astn.a@schraverus.com>
2023-03-05 11:46:21 +0100

    .

--- pareto/src/data/api.data.ts
+++ pareto/src/data/api.data.ts
@@ -7,6 +7,6 @@ const d = pd.d
 
 export const $: gapi.T.API<pd.SourceLocation> = {
     'algorithms': d({
-        "localeIsABeforeB": algorithm(functionReference("this", {}, "IsABeforeB"))
-    })
+        "localeIsABeforeB": algorithm(functionReference("this", {}, "IsABeforeB")),
+    }),
 }
